10313335de8ad5d45b82ee75a1c07dece38e5d5b
Chanchal Mishra(chanfool21) <chan123mishra@gmail.com>
2019-04-26 11:14:12 +0530

    Changed the directory structure and added NodeJS server support to handle javascript functionality

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-var tmp = require('cat-me');
 
-console.log(tmp());
+console.log("Express app");
+var fs = require('fs');
+var express = require("express");
+app = express();
+app.use(express.static('C:/Users/ftw/Desktop/HTML_DB/static/'));
+app.get("/", function(req,res){
+	res.send("Dak off");
+	});
+app.get("/des", function(req,res){
+	res.sendFile(__dirname+"/static/home.html");
+	});
+app.listen(8080, function()
+	{
+	console.log("Server has started");
+	});
+/*
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/DB", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+});
+*/
